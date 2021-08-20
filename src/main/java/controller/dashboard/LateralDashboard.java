d83dbcf0b71982bd58a1559c362e140852ae181e
Space Codee <spacecodee@gmail.com>
2021-08-19 23:48:39 -0500

    Add new sections in lateral dashboard

--- src/main/java/controller/dashboard/LateralDashboard.java
+++ src/main/java/controller/dashboard/LateralDashboard.java
@@ -1,16 +1,19 @@
 package controller.dashboard;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Cursor;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
 import utils.Images;
 
 import java.net.URL;
